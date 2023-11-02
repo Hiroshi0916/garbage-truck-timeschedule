44c59c957289f6c4126425ad8ea4d1d58da64a72
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-11-02 01:06:43 -0700

    add button

--- map-pin-app/frontend/src/Navbar.tsx
+++ map-pin-app/frontend/src/Navbar.tsx
@@ -1,22 +1,22 @@
-// Navbar.tsx
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+// 仮の管理者フラグ（実際のアプリでは、このフラグは認証状態やユーザーロールに基づいて決定されるかもしれません）
+const isAdmin = true; // これを動的に設定する必要がある場合は、propsやstateを通じて設定してください
 
 const Navbar = () => {
-
-
-
-
   return (
     <nav className="navbar" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#333', color: 'white', padding: '10px 20px' }}>
       <h1 className="navbar-logo">My Application</h1>
       <div>
         {/* トップページへのリンク */}
-        {/* <Link className="menu-item" to="/" style={{ color: 'white', padding: '0 10px', textDecoration: 'none' }}>Home</Link> */}
+        <Link className="menu-item" to="/" style={{ color: 'white', padding: '0 10px', textDecoration: 'none' }}>Home</Link>
+        
+        {/* ユーザー情報登録ページへのリンク */}
+        <Link className="menu-item" to="/user-registration" style={{ color: 'white', padding: '0 10px', textDecoration: 'none' }}>Register</Link>
+
         {/* 管理者向けボタン（条件に応じて表示） */}
-        {/* isAdmin && <Link className="menu-item" to="/admin" style={{ color: 'white', padding: '0 10px', textDecoration: 'none' }}>Admin</Link> */}
-        {/* 上記の条件に基づいてコメントアウトを外してください */}
+        {isAdmin && <Link className="menu-item" to="/admin" style={{ color: 'white', padding: '0 10px', textDecoration: 'none' }}>Admin</Link>}
       </div>
     </nav>
   );
