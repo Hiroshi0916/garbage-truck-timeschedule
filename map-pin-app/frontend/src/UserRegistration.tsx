ed637903a5a7ad23d4d421cf28665f990a5deb22
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-11-04 00:45:42 -0700

    update user registration

--- map-pin-app/frontend/src/UserRegistration.tsx
+++ map-pin-app/frontend/src/UserRegistration.tsx
@@ -1,92 +1,126 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import "./UserRegistration";
+
+interface Errors {
+  userName?: string;
+  postalCode?: string;
+  address?: string;
+}
+
+interface User {
+  userName: string;
+  postalCode: string;
+  address: string;
+}
 
 const UserRegistration = () => {
-  const [userName, setUserName] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [postalCode, setPostalCode] = useState('');
-  const [address, setAddress] = useState('');
-  const [streetNumber, setStreetNumber] = useState('');
-  const [errors, setErrors] = useState<string[]>([]);
-  const [showModal, setShowModal] = useState(false);
+  const [userName, setUserName] = useState<string>("");
+  const [postalCode, setPostalCode] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [errors, setErrors] = useState<Errors>({});
+  const [users, setUsers] = useState<User[]>([]); // ユーザーリストの状態
+
+  // コンポーネントがマウントされた時にローカルストレージからデータを読み込む
+  useEffect(() => {
+    const storedUsers = localStorage.getItem("users");
+    if (storedUsers) {
+      setUsers(JSON.parse(storedUsers));
+    }
+  }, []);
+
+  // ユーザーリストが変更された時にローカルストレージを更新する
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
 
   const validateForm = () => {
-    const newErrors: string[] = [];
+    let formIsValid = true;
+    let tempErrors: Errors = {};
 
-    if (!userName.trim()) newErrors.push('ユーザー名を入力してください。');
-    if (!firstName.trim()) newErrors.push('名を入力してください。');
-    if (!lastName.trim()) newErrors.push('姓を入力してください。');
-    if (!postalCode.trim()) newErrors.push('郵便番号を入力してください。');
-    if (!address.trim()) newErrors.push('住所を入力してください。');
-    if (!streetNumber.trim()) newErrors.push('番地を入力してください。');
+    if (!userName.trim()) {
+      formIsValid = false;
+      tempErrors.userName = "ユーザー名を入力してください。";
+    }
+    if (!postalCode.trim()) {
+      formIsValid = false;
+      tempErrors.postalCode = "郵便番号を入力してください。";
+    }
+    if (!address.trim()) {
+      formIsValid = false;
+      tempErrors["address"] = "住所を入力してください。";
+    }
 
-    setErrors(newErrors);
-    return newErrors.length === 0;
+    setErrors(tempErrors);
+    return formIsValid;
   };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     if (validateForm()) {
-      setShowModal(true);
+      const newUser: User = { userName, postalCode, address };
+      setUsers([...users, newUser]);
+      // フォームフィールドをクリアする
+      setUserName("");
+      setPostalCode("");
+      setAddress("");
     }
   };
 
   return (
     <div>
-      <h1>ユーザー情報登録</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-          placeholder="ユーザー名"
-        />
-        <input
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          placeholder="名"
-        />
-        <input
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          placeholder="姓"
-        />
-        <input
-          value={postalCode}
-          onChange={(e) => setPostalCode(e.target.value)}
-          placeholder="郵便番号"
-        />
-        <input
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="住所"
-        />
-        <input
-          value={streetNumber}
-          onChange={(e) => setStreetNumber(e.target.value)}
-          placeholder="番地"
-        />
+        <div>
+          <label>ユーザー名:</label>
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          {errors.userName && <div className="error">{errors.userName}</div>}
+        </div>
+        <div>
+          <label>郵便番号:</label>
+          <input
+            type="text"
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+          />
+          {errors.postalCode && (
+            <div className="error">{errors.postalCode}</div>
+          )}
+        </div>
+        <div>
+          <label>住所:</label>
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          {errors.address && <div className="error">{errors.address}</div>}
+        </div>
         <button type="submit">登録</button>
       </form>
-      {errors.length > 0 && (
-        <ul>
-          {errors.map((error, index) => (
-            <li key={index} style={{ color: 'red' }}>
-              {error}
-            </li>
-          ))}
-        </ul>
+
+      {users.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>ユーザー名</th>
+              <th>郵便番号</th>
+              <th>住所</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{user.userName}</td>
+                <td>{user.postalCode}</td>
+                <td>{user.address}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
-      {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={() => setShowModal(false)}>
-              &times;
-            </span>
-            <p>登録完了</p>
-          </div>
-        </div>
-      )}
-      {/* Add the appropriate CSS for the modal in your CSS file */}
     </div>
   );
 };
