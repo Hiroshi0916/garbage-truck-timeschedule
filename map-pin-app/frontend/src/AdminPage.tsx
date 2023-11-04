0be65d19ff1e3c77e3abb108053c42aaabf36275
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-11-03 23:13:04 -0700

    admin components update

--- map-pin-app/frontend/src/AdminPage.tsx
+++ map-pin-app/frontend/src/AdminPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import GoogleMapDisplay from './GoogleMapDisplay';
 
 const AdminPage = () => {
-    // 初期状態でローカルストレージからアドレスを読み込む
+
     const [addresses, setAddresses] = useState<string[]>(
       JSON.parse(localStorage.getItem('addresses') || '[]')
     );
@@ -25,6 +25,27 @@ const AdminPage = () => {
       }
     };
 
+        // 住所リストを表形式で表示する関数
+        const renderAddressTable = () => {
+          return (
+              <table>
+                  <thead>
+                      <tr>
+                          <th>登録住所</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {addresses.map((address, index) => (
+                          <tr key={index}>
+                              <td>{address}</td>
+                          </tr>
+                      ))}
+                  </tbody>
+              </table>
+          );
+      };
+
+
   return (
     <div>
       <input
@@ -33,6 +54,7 @@ const AdminPage = () => {
         onChange={(e) => setCurrentAddress(e.target.value)}
       />
       <button onClick={addAddress}>住所を追加</button>
+      {renderAddressTable()} {/* この行を追加 */}
       <GoogleMapDisplay addresses={addresses} />
     </div>
   );
