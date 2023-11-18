3a06b7cecf0565b59d43c48f5a062b347eb7adea
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-11-17 23:44:40 -0800

    upload files

--- map-pin-app/frontend/src/Direction.tsx
+++ map-pin-app/frontend/src/Direction.tsx
@@ -1,28 +1,32 @@
-import React, { useState, useCallback } from 'react';
-import { DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
-
-interface Location {
-  lat: number;
-  lng: number;
-}
-
-const origin: Location = { lat: 42.755955, lng: 141.32816 };
-const destination: Location = { lat: 45.299023, lng: 141.65308 };
-
-const transitPoints = [
-  { location: { lat: 43.66406, lng: 142.85445 }, stopover: true },
-  { location: { lat: 43.906742, lng: 144.79872 } },
-  { location: { lat: 43.286533, lng: 143.18524 } },
-];
+import React, { useState, useCallback } from "react";
+import { DirectionsRenderer, DirectionsService } from "@react-google-maps/api";
 
 export default function Direction() {
-  const [currentDirection, setCurrentDirection] = useState<google.maps.DirectionsResult | null>(null);
+  const origin = { lat: 35.681236, lng: 139.767125 }; // 東京駅
+  const destination = { lat: 35.658581, lng: 139.745433 }; // 東京タワー近く（目的地として設定）
 
-  const directionsCallback = useCallback((response: google.maps.DirectionsResult | null, status: google.maps.DirectionsStatus) => {
-    if (status === google.maps.DirectionsStatus.OK && response) {
-      setCurrentDirection(response);
-    } else {
-      console.error(`Error fetching directions: ${status}`);
+  const transitPoints = [
+    { location: { lat: 35.690921, lng: 139.700258 }, stopover: true }, // 新宿駅
+    { location: { lat: 35.699693, lng: 139.70622 }, stopover: true }, // 渋谷駅
+    { location: { lat: 35.713768, lng: 139.777254 }, stopover: true }, // 上野駅
+    { location: { lat: 35.707438, lng: 139.663835 }, stopover: true }, // 品川駅
+    { location: { lat: 35.728926, lng: 139.71038 }, stopover: true }, // 池袋駅
+    { location: { lat: 35.689634, lng: 139.692101 }, stopover: true }, // 新大久保駅
+    { location: { lat: 35.6895, lng: 139.691706 }, stopover: true }, // 大久保駅
+    { location: { lat: 35.699855, lng: 139.77235 }, stopover: true }, // 御徒町駅
+    { location: { lat: 35.698683, lng: 139.773619 }, stopover: true }, // 上野広小路駅
+    { location: { lat: 35.706646, lng: 139.756749 }, stopover: true }, // 御成門駅
+  ];
+
+  const [currentDirection, setCurrentDirection] = useState(null);
+
+  const directionsCallback = useCallback((response) => {
+    if (response !== null) {
+      if (response.status === "OK") {
+        setCurrentDirection(response);
+      } else {
+        console.log("Directions request failed due to " + response.status);
+      }
     }
   }, []);
 
@@ -33,7 +37,6 @@ export default function Direction() {
           origin,
           destination,
           travelMode: google.maps.TravelMode.DRIVING,
-          optimizeWaypoints: true,
           waypoints: transitPoints,
         }}
         callback={directionsCallback}
