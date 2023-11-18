dc54bd271b5d8a5876a62fda9751dbba20097e47
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-11-17 23:39:45 -0800

    upload files

--- map-pin-app/frontend/src/Direction.tsx
+++ map-pin-app/frontend/src/Direction.tsx
@@ -2,19 +2,19 @@ import React, { useState, useCallback } from 'react';
 import { DirectionsService, DirectionsRenderer, DirectionsResult, DirectionsStatus } from '@react-google-maps/api';
 
 interface Location {
-  lat: number;
-  lng: number;
-}
-
-const origin: Location = { lat: 42.755955, lng: 141.32816 };
-const destination: Location = { lat: 45.299023, lng: 141.65308 };
-
-const transitPoints = [
-  { location: { lat: 43.66406, lng: 142.85445 }, stopover: true },
-  { location: { lat: 43.906742, lng: 144.79872 } },
-  { location: { lat: 43.286533, lng: 143.18524 } },
-];
-
+    lat: number;
+    lng: number;
+  }
+  
+  const origin: Location = { lat: 42.755955, lng: 141.32816 };
+  const destination: Location = { lat: 45.299023, lng: 141.65308 };
+  
+  const transitPoints = [
+    { location: { lat: 43.66406, lng: 142.85445 }, stopover: true },
+    { location: { lat: 43.906742, lng: 144.79872 } },
+    { location: { lat: 43.286533, lng: 143.18524 } },
+  ];
+  
 export default function Direction() {
     const [currentDirection, setCurrentDirection] = useState<DirectionsResult | null>(null);
   
