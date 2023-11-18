bfff40a02463c200974be491636b718a97f063ff
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-11-17 23:37:46 -0800

    upload files

--- map-pin-app/frontend/src/App.tsx
+++ map-pin-app/frontend/src/App.tsx
@@ -1,169 +1,182 @@
-import React, { useCallback, useState } from "react";
-import {
-  GoogleMap,
-  LoadScript,
-} from "@react-google-maps/api";
-import Navbar from "./Navbar";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserRegistration from "./UserRegistration";
-import AdminPage from "./AdminPage";
-import UserProfile from "./UserProfile";
-import UserEditForm from "./UserEditForm";
+// import React, { useCallback, useState } from "react";
+// import {
+//   GoogleMap,
+//   LoadScript,
+// } from "@react-google-maps/api";
+// import Navbar from "./Navbar";
+// import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import UserRegistration from "./UserRegistration";
+// import AdminPage from "./AdminPage";
+// import UserProfile from "./UserProfile";
+// import UserEditForm from "./UserEditForm";
 
 
 
-const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+// const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
-const defaultPosition = {
-  lat: 35.681236,
-  lng: 139.767125,
-};
+// const defaultPosition = {
+//   lat: 35.681236,
+//   lng: 139.767125,
+// };
+
+// function App() {
+//   const mapContainerStyle = {
+//     width: "100%",
+//     height: "400px",
+//   };
+//   const [address, setAddress] = useState("");
+//   const [postalCode, setPostalCode] = useState("");
+//   const [lat, setLat] = useState<number | null>(null);
+//   const [lng, setLng] = useState<number | null>(null);
+
+//   const [position, setPosition] = useState<
+//     { lat: number; lng: number } | undefined
+//   >(defaultPosition);
+
+
+
+//   const handleSearch = async () => {
+//     console.log("Sending address:", address);
+//     try {
+//       const response = await fetch(
+//         `${BASE_URL}/geocode?address=${address || postalCode}`,
+//         {
+//           method: "GET", // GETリクエストでバックエンドに送信
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+
+//       const data = await response.json();
+
+//       if (data.status === "OK") {
+//         const location = data.results[0].geometry.location;
+//         console.log(location);
+//         setPosition({ lat: location.lat, lng: location.lng });
+//         setLat(location.lat);
+//         setLng(location.lng);
+//       } else {
+//         console.error("Error fetching coordinates:", data.status);
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }
+//   };
+
+//   const getCurrentLocation = useCallback(async () => {
+//     try {
+//       const response = await fetch("/current-location");
+
+//       if (!response.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+
+//       const data = await response.json();
+
+//       if (data.location) {
+//         setPosition({ lat: data.location.lat, lng: data.location.lng });
+//         setLat(data.location.lat);
+//         setLng(data.location.lng);
+//       } else {
+//         console.error("Error fetching current location");
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//     }
+//   }, []);
+
+//   // マップ読み込み時の処理
+//   const handleMapLoad = (map: any) => {
+//     console.log("Position:", position);
+
+//     console.log("Google Map is loaded!", map);
+//   };
+
+//   const handleMapClick = (e: any) => {
+//     console.log("Map clicked at:", e.latLng.toString());
+//   };
+
+
+
+//   return (
+//     <LoadScript
+//       googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""}
+//     >
+//       <Router>
+//         <Navbar />
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <div className="App-layout">
+//                 <div className="App-search-container">
+//                   <div className="App-input-group">
+//                     <label>住所:</label>
+//                     <input
+//                       className="App-input"
+//                       value={address}
+//                       onChange={(e) => setAddress(e.target.value)}
+//                     />
+//                   </div>
+//                   <div className="App-input-group">
+//                     <label>郵便番号:</label>
+//                     <input
+//                       className="App-input"
+//                       value={postalCode}
+//                       onChange={(e) => setPostalCode(e.target.value)}
+//                     />
+//                   </div>
+//                   <button className="App-button" onClick={getCurrentLocation}>
+//                     現在地を取得
+//                   </button>
+//                   <button className="App-button" onClick={handleSearch}>
+//                     検索
+//                   </button>
+//                   <div className="App-coordinates">
+//                     緯度: {lat ? lat.toFixed(6) : "N/A"}
+//                     <br />
+//                     経度: {lng ? lng.toFixed(6) : "N/A"}
+//                   </div>
+//                 </div>
+
+//                 <div className="App-map-container">
+//                   <GoogleMap
+//                     mapContainerStyle={mapContainerStyle}
+//                     center={position}
+//                     zoom={13}
+//                     onLoad={handleMapLoad}
+//                     onClick={handleMapClick}
+//                   >
+
+//                   </GoogleMap>
+//                 </div>
+//               </div>
+//             }
+//           />
+
+//           <Route path="/user-registration" element={<UserRegistration />} />
+//           <Route path="/admin" element={<AdminPage />} />
+//           <Route path="/user/profile" element={<UserProfile />} />
+//           <Route path="/user/edit" element={<UserEditForm />} />
+//         </Routes>
+//       </Router>
+//     </LoadScript>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import GoogleMapComponent from './GoogleMapComponent';
 
 function App() {
-  const mapContainerStyle = {
-    width: "100%",
-    height: "400px",
-  };
-  const [address, setAddress] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [lat, setLat] = useState<number | null>(null);
-  const [lng, setLng] = useState<number | null>(null);
-
-  const [position, setPosition] = useState<
-    { lat: number; lng: number } | undefined
-  >(defaultPosition);
-
-
-
-  const handleSearch = async () => {
-    console.log("Sending address:", address);
-    try {
-      const response = await fetch(
-        `${BASE_URL}/geocode?address=${address || postalCode}`,
-        {
-          method: "GET", // GETリクエストでバックエンドに送信
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-
-      if (data.status === "OK") {
-        const location = data.results[0].geometry.location;
-        console.log(location);
-        setPosition({ lat: location.lat, lng: location.lng });
-        setLat(location.lat);
-        setLng(location.lng);
-      } else {
-        console.error("Error fetching coordinates:", data.status);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
-  const getCurrentLocation = useCallback(async () => {
-    try {
-      const response = await fetch("/current-location");
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-
-      if (data.location) {
-        setPosition({ lat: data.location.lat, lng: data.location.lng });
-        setLat(data.location.lat);
-        setLng(data.location.lng);
-      } else {
-        console.error("Error fetching current location");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }, []);
-
-  // マップ読み込み時の処理
-  const handleMapLoad = (map: any) => {
-    console.log("Position:", position);
-
-    console.log("Google Map is loaded!", map);
-  };
-
-  const handleMapClick = (e: any) => {
-    console.log("Map clicked at:", e.latLng.toString());
-  };
-
-
-
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""}
-    >
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="App-layout">
-                <div className="App-search-container">
-                  <div className="App-input-group">
-                    <label>住所:</label>
-                    <input
-                      className="App-input"
-                      value={address}
-                      onChange={(e) => setAddress(e.target.value)}
-                    />
-                  </div>
-                  <div className="App-input-group">
-                    <label>郵便番号:</label>
-                    <input
-                      className="App-input"
-                      value={postalCode}
-                      onChange={(e) => setPostalCode(e.target.value)}
-                    />
-                  </div>
-                  <button className="App-button" onClick={getCurrentLocation}>
-                    現在地を取得
-                  </button>
-                  <button className="App-button" onClick={handleSearch}>
-                    検索
-                  </button>
-                  <div className="App-coordinates">
-                    緯度: {lat ? lat.toFixed(6) : "N/A"}
-                    <br />
-                    経度: {lng ? lng.toFixed(6) : "N/A"}
-                  </div>
-                </div>
-
-                <div className="App-map-container">
-                  <GoogleMap
-                    mapContainerStyle={mapContainerStyle}
-                    center={position}
-                    zoom={13}
-                    onLoad={handleMapLoad}
-                    onClick={handleMapClick}
-                  >
-
-                  </GoogleMap>
-                </div>
-              </div>
-            }
-          />
-
-          <Route path="/user-registration" element={<UserRegistration />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/user/profile" element={<UserProfile />} />
-          <Route path="/user/edit" element={<UserEditForm />} />
-        </Routes>
-      </Router>
-    </LoadScript>
+    <div>
+      <GoogleMapComponent />
+    </div>
   );
 }
 
---
+++ map-pin-app/frontend/src/Direction.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useCallback } from 'react';
+import { DirectionsService, DirectionsRenderer, DirectionsResult, DirectionsStatus } from '@react-google-maps/api';
+
+interface Location {
+  lat: number;
+  lng: number;
+}
+
+const origin: Location = { lat: 42.755955, lng: 141.32816 };
+const destination: Location = { lat: 45.299023, lng: 141.65308 };
+
+const transitPoints = [
+  { location: { lat: 43.66406, lng: 142.85445 }, stopover: true },
+  { location: { lat: 43.906742, lng: 144.79872 } },
+  { location: { lat: 43.286533, lng: 143.18524 } },
+];
+
+export default function Direction() {
+  const [currentDirection, setCurrentDirection] = useState<DirectionsResult | null>(null);
+
+  const directionsCallback = useCallback((response: DirectionsResult | null, status: DirectionsStatus) => {
+    if (status === 'OK' && response) {
+      setCurrentDirection(response);
+    } else {
+      console.error(`Error fetching directions ${response}`);
+    }
+  }, []);
+
+  return (
+    <>
+      <DirectionsService
+        options={{
+          origin,
+          destination,
+          travelMode: 'DRIVING',
+          optimizeWaypoints: true,
+          waypoints: transitPoints,
+        }}
+        callback={directionsCallback}
+      />
+      {currentDirection && (
+        <DirectionsRenderer
+          options={{
+            directions: currentDirection,
+          }}
+        />
+      )}
+    </>
+  );
+}
