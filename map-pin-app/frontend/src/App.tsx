90eb9c9b2ea302c9ae453cf1eed3d5239dd06312
Hiroshi0916 <hiroshi.obata@gmail.com>
2023-11-21 23:26:08 -0800

    update front codes

--- map-pin-app/frontend/src/App.tsx
+++ map-pin-app/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import Navbar from "./Navbar";
 import "./App.css";
@@ -8,6 +8,9 @@ import AdminPage from "./AdminPage";
 import { SearchForm } from "./SearchForm";
 
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const [googleMap, setGoogleMap] = useState<GoogleMap | null>(null);
+
+const googleMapRef = useRef<google.maps.Map>();
 
 function App() {
   const getCurrentLocation = useCallback(async () => {
@@ -57,35 +60,57 @@ function App() {
   };
   const defaultPosition = { lat: 35.681236, lng: 139.767125 }; // 東京駅
 
-  return (
-    <LoadScript
-      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""}
-    >
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="App-layout">
-                <SearchForm
-                  onSearch={handleSearch}
-                  onGetCurrentLocation={getCurrentLocation}
-                />
+  const googleMapRef = useRef<google.maps.Map>(); // Ref for the Google Map instance
 
-                <GoogleMap
-                   key={window.location.pathname} // ルートのパスをキーとして使用
-                  mapContainerStyle={mapContainerStyle}
-                  center={defaultPosition}
-                  zoom={10}
-                >
-                  <Direction />
-                </GoogleMap>
-              </div>
-            }
-          />
-          <Route path="/admin" element={<AdminPage />} />
-        </Routes>
+  const onGoogleMapMounted = useCallback((map) => {
+    googleMapRef.current = map;
+    setGoogleMap(map); // ステートを更新
+  }, []);
+
+
+  const onGoogleMapUnmounted = useCallback(() => {
+    googleMapRef.current = null;
+    setGoogleMap(null); // ステートをクリア
+  }, []);
+
+  useEffect(() => {
+    const onGoogleMapMounted = (map) => {
+      googleMapRef.current = map;
+    };
+
+    // GoogleMap コンポーネントがアンマウントされたら、DirectionsRenderer コンポーネントをレンダリングしない
+    const onGoogleMapUnmounted = () => {
+      googleMapRef.current = null;
+    };
+
+    googleMapRef.current = new GoogleMap({
+      key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+      mapContainerStyle,
+      center: defaultPosition,
+      zoom: 10,
+    });
+
+    googleMapRef.current.on("mount", onGoogleMapMounted);
+    googleMapRef.current.on("unmount", onGoogleMapUnmounted);
+    return () => {
+      googleMapRef.current.off("mount", onGoogleMapMounted);
+      googleMapRef.current.off("unmount", onGoogleMapUnmounted);
+    };
+  }, [defaultPosition, mapContainerStyle]); // Include dependencies
+
+  return (
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ""}>
+      <Router>
+        {/* 省略された他のコンポーネント... */}
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={defaultPosition}
+          zoom={10}
+          onLoad={onGoogleMapMounted}
+          onUnmount={onGoogleMapUnmounted}
+        >
+          <Direction googleMap={googleMapRef.current} />
+        </GoogleMap>
       </Router>
     </LoadScript>
   );
